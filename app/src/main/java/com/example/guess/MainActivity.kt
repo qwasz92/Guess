86680a7201bba7e7dae52af941275a2d5194986e
Hank Tom <lovevul35j6@gmail.com>
2020-02-16 16:09:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/guess/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.guess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    val secretNumber = SecretNumber()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("MainActivity","secret:" + secretNumber.secret)
+    }
+    fun check(view:View){
+       var n = ed_number.text.toString().toInt()
+        println("number${n}")
+        Log.d("MainActivity","number"+n)
+        val diff = secretNumber.validate(n)
+        var message = "猜對了"
+        if (diff<0){
+            message = "大一點"
+        } else if (diff>0){
+            message = "小一點"
+        }
+//        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+        AlertDialog.Builder(this).
+            setTitle("Message").
+            setMessage(message).
+            setPositiveButton("Ok",null).
+            show()
+    }
+}
